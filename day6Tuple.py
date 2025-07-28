132e9bd9714597189dd64402d48576a406f9113c
Mer-World <merawitdejene55@gmail.com>
2025-07-28 09:09:35 +0300

    done some exercises on tuples

---
+++ day6Tuple.py
@@ -0,0 +1,31 @@
+#ex-1  
+#1
+empty_tuple = ()
+#2
+brothers=('Isreal','kerod','eluid')
+sisters=('Tsionawit','Jeswit','Shalom')
+#3
+siblings = brothers + sisters
+#4
+print(len(siblings))  #6
+#5
+family_members=list(siblings)
+family_members.extend(['Dejene','Konjit'])
+print(family_members)
+
+#ex-2
+#1
+print(family_members[0:6]) #siblings
+print(family_members[-2:]) #family
+
+#2
+fruits=('banana','mango')
+vegetables=('tomato','potato')
+animal_products=('milk','meat')
+food_stuff_tp=fruits+vegetables+animal_products
+#3
+
+
+
+
+
