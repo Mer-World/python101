f42a4807767551740e2e3f22d73386574ba36ea1
Mer-World <merawitdejene55@gmail.com>
2025-07-28 09:25:00 +0300

    finished day 6

--- day6Tuple.py
+++ day6Tuple.py
@@ -24,8 +24,31 @@ vegetables=('tomato','potato')
 animal_products=('milk','meat')
 food_stuff_tp=fruits+vegetables+animal_products
 #3
+food_stuff_lt=list(food_stuff_tp)
+#4
+length = len(food_stuff_lt)
+half=length//2
+if length%2 == 0:
+    middle_items=food_stuff_lt[half-1:half+1]
+else:
+    middle_items=[food_stuff_lt[half]]
 
+print("Middle item(s): ", middle_items)
+#5
+print(food_stuff_lt[0:half+1]) # First half
+print(food_stuff_lt[half:]) # Second half
+#6
+del food_staff_tp # Deleting the tuple
+#7
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 
+if 'Estonia' in nordic_countries:
+    print("Estonia is a Nordic country")
+else:
+    print("Estonia is not a Nordic country")
 
-
+if 'Iceland' in nordic_countries:
+    print("Iceland is a Nordic country")
+else:
+    print("Iceland is not a Nordic country")
 
